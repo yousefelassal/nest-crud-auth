bbfe42255f84ca930296a2a5ca25e13a60adfbc1
Yousef Elassal <yousefsameh24@gmail.com>
2024-08-01 12:12:10 +0300

    handle crud operations through db repos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: 5432,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      entities: [User],
+      synchronize: true,
+      logging: true,
+      ssl: true,
+    }),
+    ConfigModule.forRoot(),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
