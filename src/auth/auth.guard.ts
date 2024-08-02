a5d08747255b7fe29ee76e9fe9c5e55937a7208b
Yousef Elassal <yousefsameh24@gmail.com>
2024-08-02 16:03:34 +0300

    feat: Implement AuthGuard for route protection

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,35 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  private getTokenFromRequest(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.getTokenFromRequest(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      request.user = payload;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
