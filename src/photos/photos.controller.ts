a6efedf365169d7934af837e88ab79edbe0b63ab
Yousef Elassal <yousefsameh24@gmail.com>
2024-08-02 19:35:46 +0300

    feat: Remove unused PhotosController from PhotosModule

--- src/photos/photos.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PhotosService } from './photos.service';
-
-@Controller('photos')
-export class PhotosController {
-  constructor(private readonly photosService: PhotosService) {}
-}
--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PhotosService } from './photos.service';
-import { PhotosController } from './photos.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from './entities/photo.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Photo])],
-  controllers: [PhotosController],
   providers: [PhotosService],
   exports: [PhotosService],
 })
