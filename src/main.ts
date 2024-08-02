612e91e8e852bc8c9c1bee03f7d61e58aea46fdc
Yousef Elassal <yousefsameh24@gmail.com>
2024-08-02 14:52:20 +0300

    chore: Update main.ts to log server URL after starting

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3000).then(() => {
+    console.log(`Server is running on http://localhost:3000`);
+  });
 }
 bootstrap();
